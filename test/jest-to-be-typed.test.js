04071014da833284de2cc14a9c337987169e9c1e
eddie <fdsa0329@naver.com>
2020-08-01 13:39:57 +0900

    :sparkles: Implement toBetTyped

---
+++ test/jest-to-be-typed.test.js
@@ -0,0 +1,23 @@
+describe('testing extended expect', () => {
+  it('tests normal types correctly', () => {
+    expect('').toBeType('string');
+    expect({}).toBeType('object');
+    expect(1).toBeType('number');
+    expect(false).toBeType('boolean');
+    expect(Symbol('foobar')).toBeType('symbol');
+    expect(() => {}).toBeType('function');
+  });
+  it('tests other types correctly', () => {
+    expect([]).toBeType('array');
+    expect(/foobar/).toBeType('regexp');
+    expect(new RegExp('foobar')).toBeType('regexp');
+    expect(null).toBeType('null');
+    expect(undefined).toBeType('undefined');
+    expect(new Map()).toBeType('map');
+    expect(new Set()).toBeType('set');
+    expect(new Date()).toBeType('date');
+  });
+  it('works with promises', () => {
+    return expect(Promise.resolve([])).resolves.toBeType('array');
+  });
+});
