344912ac8a282fe88fda18a1c95b70b69d890812
eddie <fdsa0329@naver.com>
2020-08-04 21:42:16 +0900

    :sparkles: Implement matchType

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
 import getType from 'jest-get-type';
 
+const TYPES = 'TYPES';
+const DEFAULT = 'DEFAULT';
+
+export const matchType = (expectedType) => {
+  if (expectedType === 'object') {
+    return TYPES;
+  }
+  return DEFAULT;
+};
+
 export const toBeTyped = (received, expected) => {
   const type = getType(received);
   const pass = type === expected;
