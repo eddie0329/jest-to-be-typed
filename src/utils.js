1e053354787cc7793c95bb3d749c2e59a85caa27
eddie <fdsa0329@naver.com>
2020-08-07 21:41:16 +0900

    :bulb: Write comments in utils

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,17 @@
 import { ADVANCED, DEFAULT } from './constants';
 
+/**
+ *
+ * @param {array} pass
+ */
 export const isPassed = (pass) => {
   return pass.filter((bool) => bool === false).length === 0;
 };
 
+/**
+ *
+ * @param {*} expectedType
+ */
 export const getExpectedMatchType = (expectedType) => {
   if (expectedType === 'object') {
     return ADVANCED;
@@ -11,5 +19,10 @@ export const getExpectedMatchType = (expectedType) => {
   return DEFAULT;
 };
 
+/**
+ *
+ * @param {object} obj1
+ * @param {object} obj2
+ */
 export const isNumberOfKeysNotEqual = (obj1, obj2) =>
   Object.keys(obj1).length !== Object.keys(obj2).length;
